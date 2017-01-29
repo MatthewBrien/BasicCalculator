17ebfe839d7bdbd4c32257f9129840089eeced8b
Matthew Brien <MatthewBrien@outlook.com>
2017-01-29 16:36:46 -0500

    Basic add, subract, multiply, and divide implemented

---
+++ BasicCalculator/CalcNumber.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+
+namespace BasicCalculator
+{
+    class CalcNumber
+    {
+        private bool decimalPoint = false;
+        private List<char> numbers = new List<char>();
+
+        public CalcNumber()
+        {
+        }
+
+        public CalcNumber(double num)
+        {
+            string input = num.ToString();
+            foreach (var charachter in input)
+            {
+                numbers.Add(charachter);
+                if(charachter == '.')
+                {
+                    decimalPoint = true;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            if(numbers.Count == 0)
+            {
+                return "0";
+            }
+            else // refactor. could be more efficient
+            {
+                char[] numberCharachters = new char[numbers.Count];
+                int i = 0;
+                foreach(var charachter in numbers)
+                {
+                    numberCharachters[i] = charachter;
+                    i++;
+                }
+                return (new string(numberCharachters));
+            }
+            
+        }
+
+        public void AddCharachter(char next)
+        {
+            if(next == '.' )
+            {
+                addDecimal();
+            }
+            else
+            {
+                numbers.Add(next);
+            }
+        }
+
+        public void RemoveCharachter()
+        {
+            if (numbers.Count > 0)
+            {
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+        }
+        
+        private void addDecimal()
+        {
+            if(decimalPoint == false)
+            {
+                numbers.Add('.');
+                decimalPoint = true;
+            }
+        }
+
+        public void Clear()
+        {
+            numbers.Clear();
+            decimalPoint = false;
+        }
+
+        public double Number()
+        {
+            return double.Parse(ToString());
+        }
+    }
+}
--- BasicCalculator/MainWindow.xaml.cs
+++ BasicCalculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Collections.Generic;
 
 namespace BasicCalculator
 {
@@ -8,32 +9,124 @@ namespace BasicCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<CalcNumber> numbers = new List<CalcNumber>();
+        char mathOperator = '+';
+
         public MainWindow()
         {
             InitializeComponent();
+            numbers.Add(new CalcNumber());
         }
 
         private void input_Click(object sender, RoutedEventArgs e)
         {
             Button callingButton = (Button)sender;
-            lblScreen.Content += callingButton.Content.ToString();
+            CurrentNumber().AddCharachter((char)callingButton.Content.ToString()[0]);
+            UpdateScreen();
         }
+
+        private void operator_Click(object sender, RoutedEventArgs e)
+        {
+            //if two numbers exist in the list, operate on them with the current opperator
+            if(numbers.Count > 1)
+            {
+                calculate();
+            }
+            else
+            {
+                numbers.Add(new CalcNumber());
+            }
+
+            Button callingButton = (Button)sender;
+            mathOperator = (char)callingButton.Content.ToString()[0];
+            UpdateScreen();
+
+        }
+
+        private void equals_Click(object sender, RoutedEventArgs e)
+        {
+            if(numbers.Count > 1)
+            {
+                calculate();
+                numbers.Add(new CalcNumber());
+            }
+        }
+
+        private void calculate()
+        {
+            if(numbers.Count > 2)
+            {
+                MessageBox.Show("Looks like there are more numbers in the list than expeccted.");
+            }
+            //get first and second numbers, and calculate
+            double num1 = numbers[0].Number();
+            numbers.RemoveAt(0);
+            double num2 = numbers[0].Number();
+            numbers.RemoveAt(0);
+            double result;
+
+            switch(mathOperator)
+            { 
+                case '+':
+                        result = num1 + num2;
+                break;
+
+                    case '-':
+                        result = num1 - num2;
+                break;
+                    case '*':
+                        result = num1 * num2;
+                break;
+                    case '/':
+                    if (num2 == 0)
+                    {
+                        result = 0;
+                    }
+                    else
+                    {
+                        result = num1 / num2;
+                    }
+                break;
+                default:
+                    MessageBox.Show("You seem to have broken something with the " + mathOperator + " symbol");
+                    result = 0; //bad practice should throw an exception
+                break;
+            }
+
+            numbers.Clear();
+            numbers.Add(new CalcNumber(result));
+            UpdateScreen();
+
+    }
         private void btnBackspace_Click(object sender, RoutedEventArgs e)
         {
-         
-            if(lblScreen.HasContent)
-            {
-                string newString = lblScreen.Content.ToString();
-                if (newString.Length > 0)
-                {
-                    lblScreen.Content = newString.Substring(0, newString.Length - 1);
-                }
-            }
+            CurrentNumber().RemoveCharachter();
+            UpdateScreen();
+           
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
-            lblScreen.Content = "";
+            numbers.Clear(); // check if this will cause a memory leak. Not sure how .net will handle
+            numbers.Add(new CalcNumber());
+            UpdateScreen();
         }
+
+        private void UpdateScreen()
+        {
+            string output = "";
+            if(numbers.Count > 1)
+            {
+                output = mathOperator + " ";
+            }
+            output += CurrentNumber().ToString();
+            lblScreen.Content = output;
+        }
+
+        private CalcNumber CurrentNumber()
+        {
+            return numbers[numbers.Count - 1];
+        }
+
     }
 }
