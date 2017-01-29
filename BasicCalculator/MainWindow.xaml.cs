e3104dccaadae0bfe822786a4b60db9cafd212c0
Matthew Brien <MatthewBrien@outlook.com>
2017-01-29 12:32:51 -0500

    Add project files.

---
+++ BasicCalculator/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BasicCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void input_Click(object sender, RoutedEventArgs e)
+        {
+            Button callingButton = (Button)sender;
+            lblScreen.Content += callingButton.Content.ToString();
+        }
+        private void btnBackspace_Click(object sender, RoutedEventArgs e)
+        {
+         
+            if(lblScreen.HasContent)
+            {
+                string newString = lblScreen.Content.ToString();
+                if (newString.Length > 0)
+                {
+                    lblScreen.Content = newString.Substring(0, newString.Length - 1);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            lblScreen.Content = "";
+        }
+    }
+}
